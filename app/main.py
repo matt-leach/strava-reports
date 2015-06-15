e6b91ba85b2780e09116e7f7a3434b7168402e1d
Matt <matt.leach25@gmail.com>
2015-06-15 17:40:06 +0100

    Adds 'fitness' view for plotting speed/HR values

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+import json
+
 from flask import Flask
 from flask import render_template
 
@@ -8,11 +10,32 @@ import secret
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello():
+@app.route("/fitness")
+def fitness():
     c = Client(access_token=secret.ACCESS_TOKEN)
-    a = c.get_athlete()
-    return render_template('index.html', name=a.firstname)
+    athlete = c.get_athlete()
+
+    activities = list(c.get_activities())
+
+    # Bit of a mess. Long run is 2. Old activities are 0, new activities are None
+    ALLOWED_ACTIVITIES = [None, 0, 2, u"0", u"2"]
+
+    activities = list(reversed([a for a in activities if a.average_heartrate and a.workout_type in ALLOWED_ACTIVITIES and a.type == "Run"]))
+
+    for a in activities:
+        if 1000 * float(a.average_speed) / (a.average_heartrate - 60) > 60:
+            print a.name
+
+    vals = [1000 * float(a.average_speed) / (a.average_heartrate - 60) for a in activities]
+    names = ["{} {}".format(a.name, a.start_date) for a in activities]
+
+    smoothed_vals = []
+    for index, val in enumerate(vals):
+        slice = vals[max(0, index-3):index+3]
+        smoothed_vals.append(sum(slice) / (0.0 + len(slice)))
+
+    data = [names, vals, smoothed_vals]
+    return render_template('index.html', data=data)
 
 if __name__ == "__main__":
     app.run(debug=True)
